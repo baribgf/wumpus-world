078dda468b183b902b640db16e9c1ae0942b9394
bari <baribgf.2023@gmail.com>
2026-01-24 13:05:36 +0100

    Updated `Statement`, and `Atomic` structs

--- src/logic.rs
+++ src/logic.rs
@@ -2,22 +2,16 @@ use std::fmt::Display;
 use std::hash::Hash;
 
 // Logic Primitives /////////////////////////////////////////
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq, Clone)]
 pub struct Atomic {
-    value: bool,
     name: String,
 }
 
 impl Atomic {
-    pub fn new(name: &str, value: bool) -> Self {
+    pub fn new(name: &str) -> Self {
         Atomic {
-            name: name.to_string(),
-            value,
+            name: name.to_string()
         }
-    }
-
-    pub fn assign(&mut self, value: bool) {
-        self.value = value;
     }
 }
 
@@ -48,27 +42,29 @@ impl UnaryOp {
 type Left = Box<Statement>;
 type Right = Box<Statement>;
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Eq, Hash, PartialEq, Clone)]
 pub enum Statement {
     Atomic(Atomic),
     AndClause(Left, Right),
     OrClause(Left, Right),
+    XorClause(Left, Right),
     NotClause(Box<Statement>),
     ImplyClause(Left, Right),
 }
 
 impl Statement {
-    fn eval(&self) -> bool {
+    pub fn eval(&self) -> bool {
         match self {
-            Statement::Atomic(atomic) => atomic.value,
-            Statement::AndClause(left, right) => left.eval() && right.eval(),
-            Statement::OrClause(left, right) => left.eval() || right.eval(),
-            Statement::NotClause(stmt) => !stmt.eval(),
-            Statement::ImplyClause(left, right) => !left.eval() | right.eval(),
+            Self::Atomic(_) => true,
+            Self::AndClause(left, right) => left.eval() & right.eval(),
+            Self::OrClause(left, right) => left.eval() | right.eval(),
+            Self::XorClause(left, right) => left.eval() ^ right.eval(),
+            Self::NotClause(stmt) => !stmt.eval(),
+            Self::ImplyClause(left, right) => !left.eval() | right.eval(),
         }
     }
 
-    fn boxed(self) -> Box<Self> {
+    pub fn boxed(self) -> Box<Self> {
         Box::new(self)
     }
 }
@@ -78,9 +74,29 @@ impl Display for Statement {
         match self {
             Statement::Atomic(atomic) => f.write_str(&atomic.name),
             Statement::AndClause(left, right) => {
-                f.write_fmt(format_args!("({} & {})", left, right))
+                let mut right_repr = format!("{}", right);
+                if let Statement::AndClause(_, _) = &**right {
+                    right_repr.remove(0);
+                    right_repr.pop();
+                }
+                f.write_fmt(format_args!("({} & {})", left, right_repr))
             }
-            Statement::OrClause(left, right) => f.write_fmt(format_args!("({} | {})", left, right)),
+            Statement::OrClause(left, right) => {
+                let mut right_repr = format!("{}", right);
+                if let Statement::OrClause(_, _) = &**right {
+                    right_repr.remove(0);
+                    right_repr.pop();
+                }
+                f.write_fmt(format_args!("({} | {})", left, right_repr))
+            },
+            Statement::XorClause(left, right) => {
+                let mut right_repr = format!("{}", right);
+                if let Statement::XorClause(_, _) = &**right {
+                    right_repr.remove(0);
+                    right_repr.pop();
+                }
+                f.write_fmt(format_args!("({} ⊕  {})", left, right_repr))
+            }
             Statement::NotClause(stmt) => f.write_fmt(format_args!("~{}", stmt)),
             Statement::ImplyClause(left, right) => {
                 f.write_fmt(format_args!("({} -> {})", left, right))
